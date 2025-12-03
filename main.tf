fbc35e953071ae74e13b62782c4086d556c15d92
chandralek.singasani <chandralek.singasani@servicenow.com>
2025-12-03 06:39:23 +0530

    roboshop project

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ module "component" {
   network_security_group_id = var.network_security_group_id
 }
 
-module "component" {
+module "mongodb" {
   source = "./component"
   component           = "mongodb"
   image_id            = var.image_id
@@ -20,7 +20,7 @@ module "component" {
   network_security_group_id = var.network_security_group_id
 }
 
-module "component" {
+module "mysql" {
   source = "./component"
   component           = "mysql"
   image_id            = var.image_id
@@ -31,7 +31,7 @@ module "component" {
   network_security_group_id = var.network_security_group_id
 }
 
-module "component" {
+module "redis" {
   source = "./component"
   component           = "redis"
   image_id            = var.image_id
@@ -42,7 +42,7 @@ module "component" {
   network_security_group_id = var.network_security_group_id
 }
 
-module "component" {
+module "rabbitmq" {
   source = "./component"
   component           = "rabbitmq"
   image_id            = var.image_id
@@ -53,7 +53,7 @@ module "component" {
   network_security_group_id = var.network_security_group_id
 }
 
-module "component" {
+module "cart" {
   source = "./component"
   component           = "cart"
   image_id            = var.image_id
@@ -64,8 +64,8 @@ module "component" {
   network_security_group_id = var.network_security_group_id
 }
 
-module "component" {
-  source = "./component"
+module "catalogue" {
+  source              = "./component"
   component           = "catalogue"
   image_id            = var.image_id
   location            = var.location
@@ -75,7 +75,7 @@ module "component" {
   network_security_group_id = var.network_security_group_id
 }
 
-module "component" {
+module "payment" {
   source = "./component"
   component           = "payment"
   image_id            = var.image_id
@@ -86,7 +86,7 @@ module "component" {
   network_security_group_id = var.network_security_group_id
 }
 
-module "component" {
+module "shipping" {
   source = "./component"
   component           = "shipping"
   image_id            = var.image_id
@@ -97,7 +97,7 @@ module "component" {
   network_security_group_id = var.network_security_group_id
 }
 
-module "component" {
+module "user" {
   source = "./component"
   component           = "user"
   image_id            = var.image_id
