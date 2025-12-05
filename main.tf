84689117c713223b7aab58baec1f777d33ce03b8
chandralek.singasani <chandralek.singasani@servicenow.com>
2025-12-05 13:27:46 +0530

    roboshop project

--- main.tf
+++ main.tf
@@ -12,8 +12,8 @@ module "databases" {
   source              = "./modules/vm"
   component           = each.key
   image_id            = var.image_id
-  location            = module.resource-rg[each.value[rgname]].rg_location
-  resource_group_name = module.resource-rg[each.value[rgname]].rg_name
+  location            = module.resource-rg[each.value["rgname"]].rg_location
+  resource_group_name = module.resource-rg[each.value["rgname"]].rg_name
   subnet_id           = var.subnet_id
   zone_name           = var.zone_name
   network_security_group_id = var.network_security_group_id
